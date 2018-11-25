d73f2805baa0e3f4627bc6be6e34a1387fb7882a
Manuel Spigolon <behemoth89@gmail.com>
2018-11-25 15:21:33 +0100

    Clean project

--- list.js
+++ list.js
@@ -1,13 +1,17 @@
-const ytpl = require('ytpl');
+'use strict';
+
+// run with:
+// npm run cli PLAv2aQ9JgGbVcUtDpuiTB9WgaMljCUpa_
+
+
 const DownloadYTFile = require('./download-audio');
 
 const downloader = new DownloadYTFile();
 
 downloader.on('progress', console.log)
 
-// https://www.youtube.com/playlist?list=PLAv2aQ9JgGbVcUtDpuiTB9WgaMljCUpa_
-// https://www.youtube.com/playlist?list=PLAv2aQ9JgGbXJQvONGorCeLZLmAP-BqwZ
-// ytpl('PLBweDmMi-NsOeiKfKKrtcNW1HNy2giuqY', function (err, playlist) {
-downloader.downloadPlaylist('PLAv2aQ9JgGbVcUtDpuiTB9WgaMljCUpa_')
+const playlist = process.argv[2];
+console.log(`Downloading ${playlist}`);
+downloader.downloadPlaylist(playlist)
   .then((res) => { console.log('Done', res) })
   .catch(console.log);
