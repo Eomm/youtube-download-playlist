ab0daceb59bf2f512e9292f337a9717f83b1921a
Manuel Spigolon <behemoth89@gmail.com>
2018-11-25 15:08:58 +0100

    Big refactor code

--- download-audio.js
+++ download-audio.js
@@ -1,45 +1,89 @@
 'use strict';
 
+const EventEmitter = require('events').EventEmitter;
 const horizon = require('horizon-youtube-mp3');
+const fue = require('file-utils-easy');
+const ytpl = require('ytpl');
 const path = require('path');
-const downloadPath = path.join(__dirname);
 
-function onConvertVideoProgress(percent, timemark, targetSize) {
-  console.log('Progress:', percent, 'Timemark:', timemark, 'Target Size:', targetSize);
-  // Will return...
-  // Progress: 90.45518257038955 Timemark: 00:02:20.04 Target Size: 2189
-  // Progress: 93.73001672942894 Timemark: 00:02:25.11 Target Size: 2268
-  // Progress: 100.0083970106642 Timemark: 00:02:34.83 Target Size: 2420
-}
+const YOUTUBE_URL = 'http://youtube.com/watch?v=';
 
 
-
-
-
-
-class DownloadYTFile {
-  constructor() {
-
+class DownloadYTFile extends EventEmitter {
+  constructor(config = {}) {
+    super();
+    this.downloadPath = config.outputPath || path.join(__dirname);
+    // this.override = config.override || true;
   }
 
   download(id, fileName = null) {
     return new Promise((resolve, reject) => {
-      horizon.downloadToLocal(`http://youtube.com/watch?v=${id}`,
-        downloadPath, fileName, null, null,
+
+      // videoURL:String,
+      // directory:String,
+      // optionalName?:String,
+      // cropParams?:Object,
+      // maxTimeAllowed?:Number,
+      // callback?:Function,
+      // onProgress?:Function
+
+
+      // const cropParams = { start: '02:15', end: '02:20' };
+
+      horizon.downloadToLocal(`${YOUTUBE_URL}${id}`,
+        this.downloadPath, fileName, null, null,
         (err, result) => {
-          console.log(err, result);
-          // Will return...
-          //null, conversionFileComplete
+          const info = { id, fileName };
+          this.emit('complete', info);
+
           if (err) {
             return reject(err);
           }
           resolve(result);
         },
-        null);
-
+        (percent, timemark, targetSize) => {
+          const info = {
+            id, fileName, percent, timemark, targetSize
+          };
+          this.emit('progress', info);
+        });
     });
   }
 
+  async downloadPlaylist(playlistId) {
+    const playlistoInfo = await this.playlistInfo(playlistId);
+
+    const sequentialDownload = async (array) => {
+      for (let i = 0; i < array.length; i++) {
+        const song = array[i];
+        try {
+          try {
+            // TODO check override setting
+            await fue.existFile(`${song.title}.mp3`);
+          } catch (error) {
+            await this.download(song.id, song.title.substr(0, 3));
+            console.log('Downloaded', song.id);
+          }
+        } catch (error) {
+          console.log('Error downloading', song.title, error);
+        }
+      }
+    };
+
+    await sequentialDownload(playlistoInfo.items);
+    return true;
+  }
+
+  playlistInfo(playlistId) {
+    return new Promise((resolve, reject) => {
+      ytpl(playlistId, (err, playlist) => {
+        if (err) {
+          return reject(err);
+        }
+        resolve(playlist);
+      });
+    });
+  }
 }
 
 module.exports = DownloadYTFile;
--- list.js
+++ list.js
@@ -1,34 +1,13 @@
 const ytpl = require('ytpl');
 const DownloadYTFile = require('./download-audio');
-const fue = require('file-utils-easy');
 
+const downloader = new DownloadYTFile();
 
+downloader.on('progress', console.log)
+
+// https://www.youtube.com/playlist?list=PLAv2aQ9JgGbVcUtDpuiTB9WgaMljCUpa_
 // https://www.youtube.com/playlist?list=PLAv2aQ9JgGbXJQvONGorCeLZLmAP-BqwZ
-ytpl('PLBweDmMi-NsOeiKfKKrtcNW1HNy2giuqY', function (err, playlist) {
-  if (err) throw err;
-  console.log(playlist);
-  console.log(playlist.items.length);
-
-  sequentialDownload(playlist.items)
-    .then(() => {
-      console.log('finito');
-    })
-});
-
-
-async function sequentialDownload(array) {
-  const d = new DownloadYTFile();
-  for (let i = 0; i < array.length; i++) {
-    const song = array[i];
-    try {
-      try {
-        await fue.existFile(`${song.title}.mp3`);
-      } catch (error) {
-        await d.download(song.id);
-        console.log('scaricato', song.id);
-      }
-    } catch (error) {
-      console.log('error for', song.title);
-    }
-  }
-}
+// ytpl('PLBweDmMi-NsOeiKfKKrtcNW1HNy2giuqY', function (err, playlist) {
+downloader.downloadPlaylist('PLAv2aQ9JgGbVcUtDpuiTB9WgaMljCUpa_')
+  .then((res) => { console.log('Done', res) })
+  .catch(console.log);
