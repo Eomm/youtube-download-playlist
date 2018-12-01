c550dcba79268a15c6be4cb3ebec6962145e2d11
Manuel Spigolon <behemoth89@gmail.com>
2018-12-01 23:03:10 +0100

    Removed electron (new repo will created) clean dependencies and standard over eslint

--- cli.js
+++ cli.js
@@ -1,17 +1,16 @@
-'use strict';
+'use strict'
 
 // run with:
 // npm run cli PLAv2aQ9JgGbVcUtDpuiTB9WgaMljCUpa_
 
+const DownloadYTFile = require('./lib/download-audio')
 
-const DownloadYTFile = require('./lib/download-audio');
+const downloader = new DownloadYTFile()
 
-const downloader = new DownloadYTFile();
+downloader.on('progress', console.log)
 
-downloader.on('progress', console.log);
-
-const playlist = process.argv[2];
-console.log(`Downloading ${playlist}`);
+const playlist = process.argv[2]
+console.log(`Downloading ${playlist}`)
 downloader.downloadPlaylist(playlist)
-  .then((res) => { console.log('Done', res); })
-  .catch(console.log);
+  .then((res) => { console.log('Done', res) })
+  .catch(console.log)
--- test/download.test.js
+++ test/download.test.js
@@ -1,56 +1,56 @@
-'use strict';
+'use strict'
 
-const DownloadYTFile = require('../lib/download-audio');
-const fue = require('file-utils-easy');
+const DownloadYTFile = require('../lib/download-audio')
+const fue = require('file-utils-easy')
 
-const OUT_DIR = 'test/out';
+const OUT_DIR = 'test/out'
 const TEST_PLAYLIST = {
   twoSongs: { id: 'PLAv2aQ9JgGbVcUtDpuiTB9WgaMljCUpa_', length: 2 },
-  hundredSongs: { id: 'PLBweDmMi-NsOeiKfKKrtcNW1HNy2giuqY', length: 100 },
-};
+  hundredSongs: { id: 'PLBweDmMi-NsOeiKfKKrtcNW1HNy2giuqY', length: 100 }
+}
 
 describe('Core lib', () => {
-  beforeAll(() => fue.deleteDirectoryFiles(OUT_DIR));
-  afterAll(() => fue.deleteDirectoryFiles(OUT_DIR));
+  beforeAll(() => fue.deleteDirectoryFiles(OUT_DIR))
+  afterAll(() => fue.deleteDirectoryFiles(OUT_DIR))
 
   it('download small playlist', async () => {
-    const downloader = new DownloadYTFile({ outputPath: OUT_DIR });
+    const downloader = new DownloadYTFile({ outputPath: OUT_DIR })
 
-    let progressEvents = 0;
-    let completeEvents = 0;
-    downloader.on('progress', () => progressEvents++);
-    downloader.on('complete', () => completeEvents++);
+    let progressEvents = 0
+    let completeEvents = 0
+    downloader.on('progress', () => progressEvents++)
+    downloader.on('complete', () => completeEvents++)
 
-    const result = await downloader.downloadPlaylist(TEST_PLAYLIST.twoSongs.id);
+    const result = await downloader.downloadPlaylist(TEST_PLAYLIST.twoSongs.id)
 
     // TODO more check on output
-    expect(result).toHaveLength(TEST_PLAYLIST.twoSongs.length);
-    expect(completeEvents).toEqual(TEST_PLAYLIST.twoSongs.length);
-    expect(progressEvents).toBeGreaterThanOrEqual(TEST_PLAYLIST.twoSongs.length);
-  }, 120000);
+    expect(result).toHaveLength(TEST_PLAYLIST.twoSongs.length)
+    expect(completeEvents).toEqual(TEST_PLAYLIST.twoSongs.length)
+    expect(progressEvents).toBeGreaterThanOrEqual(TEST_PLAYLIST.twoSongs.length)
+  }, 120000)
 
   it('download huge playlist', async () => {
     // TODO implement
-    expect(true).toEqual(false);
-  });
+    expect(true).toEqual(false)
+  })
 
   it('download single song', async () => {
     // TODO implement
-    expect(true).toEqual(false);
-  });
+    expect(true).toEqual(false)
+  })
 
   it('get playlist info', async () => {
     // TODO implement
-    expect(true).toEqual(false);
-  });
+    expect(true).toEqual(false)
+  })
 
   it('override parameter test', async () => {
     // TODO implement
-    expect(true).toEqual(false);
-  });
+    expect(true).toEqual(false)
+  })
 
   it('ffmpeg controls', async () => {
     // TODO implement
-    expect(true).toEqual(false);
-  });
-});
+    expect(true).toEqual(false)
+  })
+})
