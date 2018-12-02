e1269cfa2493c25dc3eb7d919ca0525bc0d99bc5
Manuel Spigolon <behemoth89@gmail.com>
2018-12-02 23:34:40 +0100

    Added ffmpeg config path

--- test/download.test.js
+++ test/download.test.js
@@ -2,11 +2,13 @@
 
 const DownloadYTAudio = require('../lib/download-audio')
 const fue = require('file-utils-easy')
+const path = require('path')
 
 const OUT_DIR = 'test/out'
 const TEST_PLAYLIST = {
   singleSong: { id: 'ZIyyj2FrVI0', length: 1 },
   twoSongs: { id: 'PLAv2aQ9JgGbVcUtDpuiTB9WgaMljCUpa_', length: 2 },
+  twoSongsDuplicated: { id: 'PLAv2aQ9JgGbVEzvdzyoigNgnJcvOtMmU0', length: 2 },
   hundredSongs: { id: 'PLBweDmMi-NsOeiKfKKrtcNW1HNy2giuqY', length: 100 }
 }
 
@@ -85,7 +87,7 @@ describe('Core lib', () => {
     expect(info).toMatchObject(compareTo)
     expect(info.url).toBeDefined()
     expect(info.thumbnail).toBeDefined()
-  })
+  }, 15000)
 
   it('get video info error', async () => {
     let error
@@ -105,7 +107,7 @@ describe('Core lib', () => {
       expect(i.id).toBeDefined()
       expect(i.title).toBeDefined()
     })
-  })
+  }, 10000)
 
   it('get playlist info error', async () => {
     let error
@@ -128,23 +130,51 @@ describe('Core lib', () => {
     downloader.on('complete', () => completeEvents++)
     downloader.on('error', () => errorEvents++)
 
+    const mockCustomFilename = jest.fn((title) => {
+      return 'overwrite.mp3'
+    })
+
     downloader.overwriteExistingFiles = false
-    const customFilename = 'overwrite.mp3'
-    const result = await downloader.download(TEST_PLAYLIST.singleSong.id, customFilename)
+    downloader.fileNameGenerator = mockCustomFilename
+    const result = await downloader.download(TEST_PLAYLIST.singleSong.id)
 
     let overwriteError
     try {
-      await downloader.download(TEST_PLAYLIST.singleSong.id, customFilename)
+      await downloader.download(TEST_PLAYLIST.singleSong.id)
     } catch (error) {
       overwriteError = error
     }
     expect(overwriteError).toBeDefined()
-
-    expect(result).toMatchObject({ fileName: customFilename, id: TEST_PLAYLIST.singleSong.id, path: OUT_DIR, result: 'conversionFileComplete' })
+    expect(mockCustomFilename).toHaveBeenCalledTimes(2)
+    expect(result).toMatchObject({ fileName: mockCustomFilename(), id: TEST_PLAYLIST.singleSong.id, path: OUT_DIR, result: 'conversionFileComplete' })
     expect(startEvents).toEqual(TEST_PLAYLIST.singleSong.length)
     expect(completeEvents).toEqual(TEST_PLAYLIST.singleSong.length)
     expect(errorEvents).toEqual(1)
     expect(progressEvents).toBeGreaterThanOrEqual(1) // high speed network, less progress event
+  }, 120000)
+
+  it('download playlist with errors', async () => {
+    let startEvents = 0
+    let progressEvents = 0
+    let completeEvents = 0
+    let errorEvents = 0
+
+    downloader.on('start', () => startEvents++)
+    downloader.on('progress', () => progressEvents++)
+    downloader.on('complete', () => completeEvents++)
+    downloader.on('error', () => errorEvents++)
+
+    const mockCustomFilename = jest.fn(() => 'error-test.mp3')
+    downloader.overwriteExistingFiles = false
+    downloader.fileNameGenerator = mockCustomFilename
+    await fue.writeToFile('', path.join(OUT_DIR, 'error-test.mp3'))
+    const result = await downloader.downloadPlaylist(TEST_PLAYLIST.twoSongsDuplicated.id)
+
+    expect(result).toHaveLength(TEST_PLAYLIST.twoSongsDuplicated.length)
+    expect(startEvents).toEqual(0)
+    expect(completeEvents).toEqual(0)
+    expect(errorEvents).toEqual(2)
+    expect(result.find((song) => song.error !== undefined)).toBeDefined()
   }, 120000)
 
   it.skip('ffmpeg controls', async () => {
