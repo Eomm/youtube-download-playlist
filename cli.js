41014c15724ea5941b2dec3086bf90daa4ef44e4
Manuel Spigolon <behemoth89@gmail.com>
2018-12-02 22:00:59 +0100

    Simple better CLI for all commands

--- cli.js
+++ cli.js
@@ -2,15 +2,34 @@
 
 // run with:
 // npm run cli PLAv2aQ9JgGbVcUtDpuiTB9WgaMljCUpa_
+// NB: this is only a simple utility, use with caution
 
 const DownloadYTFile = require('./lib/download-audio')
 
-const downloader = new DownloadYTFile()
+const id = process.argv[2]
+const command = process.argv[3] || 'playlist'
 
-downloader.on('progress', console.log)
+const validCommand = new Map([
+  ['playlist', 'downloadPlaylist'],
+  ['video', 'download'],
+  ['info-video', 'getVideoInfo'],
+  ['info-playlist', 'getPlaylistInfo']
+])
 
-const playlist = process.argv[2]
-console.log(`Downloading ${playlist}`)
-downloader.downloadPlaylist(playlist)
+if (!validCommand.has(command)) {
+  console.log(`The command ${command} is not valid, use one of: ${Array.from(validCommand.keys())}`)
+  process.exit(-1)
+}
+
+console.log(`Executing ${command} - ${id}`)
+const commandExecution = validCommand.get(command)
+
+const downloader = new DownloadYTFile({ outputPath: __dirname })
+
+downloader.on('start', console.log)
+downloader.on('progress', (_) => console.log(_.percent))
+downloader.on('error', console.log)
+
+downloader[commandExecution](id)
   .then((res) => { console.log('Done', res) })
   .catch(console.log)
