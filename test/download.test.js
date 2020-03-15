1d1aafee1051940d5d2c344718f639bdbde88e7b
Manuel Spigolon <behemoth89@gmail.com>
2020-03-15 16:01:20 +0100

    add ci

--- test/download.test.js
+++ test/download.test.js
@@ -28,3 +28,59 @@ t.test('get video info', async t => {
   t.ok(info.thumbnail)
   t.ok(info.raw)
 })
+
+t.test('get video info error', async t => {
+  try {
+    const downloader = new DownloadYTAudio({ outputPath: OUT_DIR })
+    await downloader.getVideoInfo('abcdefg8761')
+    t.fail('shold now succede')
+  } catch (err) {
+    t.ok(err)
+    t.like(err.message, 'This video is unavailable')
+  }
+})
+
+t.test('get playlist info', async t => {
+  const downloader = new DownloadYTAudio({ outputPath: OUT_DIR })
+  const info = await downloader.getPlaylistInfo(TEST_PLAYLIST.hundredSongs.id)
+
+  t.equals(info.id, TEST_PLAYLIST.hundredSongs.id)
+  t.equals(info.items.length, TEST_PLAYLIST.hundredSongs.length)
+  info.items.forEach((i) => {
+    t.ok(i.id)
+    t.ok(i.title)
+  })
+})
+
+t.test('get playlist info error', async t => {
+  try {
+    const downloader = new DownloadYTAudio({ outputPath: OUT_DIR })
+    await downloader.getPlaylistInfo('abcdefg8761')
+    t.fail('shold now succede')
+  } catch (err) {
+    t.ok(err)
+    t.like(err.message, 'Unable to find a id in abcdefg8761')
+  }
+})
+
+t.test('download single song', async t => {
+  let startEvents = 0
+  let progressEvents = 0
+  let completeEvents = 0
+  let errorEvents = 0
+
+  const downloader = new DownloadYTAudio({ outputPath: OUT_DIR })
+  downloader.on('start', () => startEvents++)
+  downloader.on('progress', () => progressEvents++)
+  downloader.on('complete', () => completeEvents++)
+  downloader.on('error', () => errorEvents++)
+
+  const result = await downloader.download(TEST_PLAYLIST.singleSong.id)
+
+  t.like(result, { id: TEST_PLAYLIST.singleSong.id, path: OUT_DIR, result: 'conversionFileComplete' })
+  t.equals(result.fileName, `${result.ref.title}.mp3`)
+  t.equals(startEvents, TEST_PLAYLIST.singleSong.length)
+  t.equals(completeEvents, TEST_PLAYLIST.singleSong.length)
+  t.ok(progressEvents >= TEST_PLAYLIST.singleSong.length)
+  t.equals(errorEvents, 0)
+})
